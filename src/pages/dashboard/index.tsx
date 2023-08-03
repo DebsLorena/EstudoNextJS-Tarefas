37576a0ec715f128b0d15ed3c035db634ee58986
DebsLorena <lorenahrd@gmail.com>
2023-08-03 08:46:17 -0300

    feat: dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,14 +1,85 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import styles from './styles.module.css'
+import { getSession } from 'next-auth/react'
+import { Textarea } from '@/components/textarea'
+import { FiShare2 } from 'react-icons/fi'
+import { FaTrash } from 'react-icons/fa'
+import { ChangeEvent, useState } from 'react'
 
 
 export default function Dashboard() {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>Meu painel de tarefas</title>
-            </Head>
-            <h1>Dahsboard</h1>
-        </div>
-    )
+
+	const [input, setInput] = useState('')
+	const [publicTask, setPublicTask] = useState(false)
+
+	function handleChangePublic(event: ChangeEvent<HTMLInputElement>) {
+		setPublicTask(event.target.checked)
+	}
+
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Meu painel de tarefas</title>
+			</Head>
+			<main className={styles.main}>
+				<section className={styles.content}>
+					<div className={styles.contentform}>
+						<h1 className={styles.title}> Qual a sua Tarefa? </h1>
+						<form>
+							<Textarea
+								placeholder='Digite qual sua tarefa...'
+								value={input}
+								onChange={(event: ChangeEvent<HTMLTextAreaElement>) => setInput(event.target.value)}
+							/>
+
+							<div className={styles.checkboxArea}>
+								<input
+									type='checkbox' className={styles.checbox}
+									checked={publicTask}
+									onChange={handleChangePublic}
+								/>
+								<label> Tarefa publica?</label>
+							</div>
+
+							<button className={styles.button} type='submit'>Registrar</button>
+						</form>
+					</div>
+				</section>
+				<section className={styles.taskContainer}>
+					<h1>Minhas tarefas</h1>
+					<article className={styles.task}>
+						<div className={styles.tagContainer}>
+							<label className={styles.tag}>PUBLICO</label>
+							<button className={styles.shareButton}>
+								<FiShare2 size={22} color="#3183ff" />
+							</button>
+						</div>
+						<div className={styles.taskContent}>
+							<p>Minha primeira tarefa de exemplo show demais!</p>
+							<button className={styles.trashButton}>
+								<FaTrash size={22} color="#ea3140" />
+							</button>
+						</div>
+					</article>
+				</section>
+			</main>
+		</div>
+	)
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+	const session = await getSession({ req })
+	console.log(session)
+	if (!session?.user) {
+		return {
+			redirect: {
+				destination: '/',
+				permanent: false,
+			}
+		}
+	}
+	return {
+		props: {}
+	}
+};
